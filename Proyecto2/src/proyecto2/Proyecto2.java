bcfa18b658aaa8df0a60cce4f75dc5e5d3b31597
Bryan Mejia <Alexis@Bryans-MacBook-Air.local>
2016-08-20 14:03:44 -0600

    Se diferencian entre Palabras y Tabulaciones

--- Proyecto2/src/proyecto2/Proyecto2.java
+++ Proyecto2/src/proyecto2/Proyecto2.java
@@ -5,6 +5,11 @@
  */
 package proyecto2;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 /**
  *
  * @author Alexis
@@ -14,8 +19,44 @@ public class Proyecto2 {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args) throws IOException {
+        muestraContenido("./Prueba.txt");
     }
     
+    
+    public static void muestraContenido(String archivo) throws FileNotFoundException, IOException {
+        String cadena;
+        
+        FileReader f = new FileReader(archivo);
+        BufferedReader b = new BufferedReader(f);
+        while ((cadena = b.readLine()) != null) {
+            //System.out.println(cadena);
+            //contadorTab(cadena);
+            System.out.println(nombrePersona(cadena,contadorTab(cadena)));
+            //obtenerCuenta(cadena);
+        }
+        b.close();
+    }
+    
+    public static int contadorTab(String x){
+        int pos1=-1;
+        int numeroTab=0;
+        int bandera=0;
+        while(bandera==0){
+            pos1=x.indexOf('\t',pos1+1);
+            if(pos1!=-1){
+                numeroTab++;
+            }else{
+                bandera=1;
+            }
+        }
+        //System.out.println("No de saltos: "+numeroTab);
+        return numeroTab;
+        
+        //System.out.println("Lugar encontrar. "+ pos1);
+    }
+    
+    public static String nombrePersona(String x, int a){
+        return x.substring(a);
+    }
 }
